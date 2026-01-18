8451af4416a9a07a906dd2f7aaa38a9bb7061575
jeswinjoseph123 <jeswinjoseph08@gmail.com>
2026-01-18 17:32:08 +0000

    taskify

---
+++ src/components/InputField.tsx
@@ -0,0 +1,34 @@
+import "./style.css";
+import { useRef } from "react";
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder="Please enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit" className="input_submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
